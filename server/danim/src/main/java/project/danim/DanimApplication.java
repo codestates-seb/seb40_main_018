e02ac74847ab383d39471fe76f9af662ec93c808
2D3 <as13337@gmail.com>
2022-11-08 17:38:46 +0900

    Add: directory

---
+++ server/danim/src/main/java/project/danim/DanimApplication.java
@@ -0,0 +1,13 @@
+package project.danim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanimApplication.class, args);
+	}
+
+}
---
+++ server/danim/src/test/java/project/danim/DanimApplicationTests.java
@@ -0,0 +1,13 @@
+package project.danim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
