6e2e95b5b5c165bc23dd85eaff17a4dc57dcef01
2D3Edith <as13337@gmail.com>
2022-11-10 22:02:31 +0900

    추가: 버킷리스트 구현 완료

--- server/danim/src/main/java/project/danim/bucket/dto/BucketPostDto.java
+++ server/danim/src/main/java/project/danim/bucket/dto/BucketPostDto.java
@@ -1,4 +1,19 @@
 package project.danim.bucket.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class BucketPostDto {
+
+    @NotBlank(message="내용을 입력해주세요.")
+    private String bucketContent;
+
+    private Boolean isBucket;
+
 }
