03c91623c29a719e01f37313a47bd07fa523de0a
2D3Edith <as13337@gmail.com>
2022-11-10 12:13:31 +0900

    Add: 공동 사용 Exeption 추가
    - 필요한 경우 수정하고 머지할 때 충돌해결 하시면 됩니다

---
+++ server/danim/src/main/java/project/danim/exeption/ExceptionCode.java
@@ -0,0 +1,35 @@
+package project.danim.exeption;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+
+    // 멤버
+    MEMBER_NOT_FOUND(404, "회원 정보를 찾을 수 없습니다."),
+    MEMBER_EXISTS(409, "회원이 이미 존재합니다."),
+    MEMBER_LOGIN_POSSIBLE(303, "모든 정보가 일치하여 로그인 가능합니다."),
+    NOT_LOGIN(403, "로그인이 되어있지 않습니다."),
+
+    // 일기
+    ARTICLE_NOT_FOUND(404, "일기가 존재하지 않습니다."),
+
+    // 댓글
+    COMMENT_EXIST(409, "존재하는 댓글입니다."),
+    COMMENT_NOT_FOUND(404, "댓글을 찾을 수 없습니다."),
+
+    // 태그
+    TAG_NOT_FOUND(404, "태그를 찾을 수 없습니다"),
+
+    ACCESS_FORBIDDEN(403, "접근 권한이 없습니다.");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
