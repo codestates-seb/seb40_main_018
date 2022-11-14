97aa76b8021a103284d3e09fcd1911568df31758
hyeonayou <Youhyeona99@gmail.com>
2022-11-15 02:25:14 +0900

    add : DiaryMapperlmpl

---
+++ server/danim/src/main/generated/project/danim/diary/mapper/DiaryMapperImpl.java
@@ -0,0 +1,13 @@
+package project.danim.diary.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-11-15T01:32:35+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class DiaryMapperImpl implements DiaryMapper {
+}
