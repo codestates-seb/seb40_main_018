3e6783ecdc5ebbbe4c33a3da7945477ff91843e5
hyeonayou <Youhyeona99@gmail.com>
2022-11-21 15:57:38 +0900

    add : likes 뼈대 생성

---
+++ server/danim/src/main/java/project/danim/likes/controller/LikesController.java
@@ -0,0 +1,31 @@
+package project.danim.likes.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Positive;
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/likes")
+public class LikesController {
+    @GetMapping("/{diary-id}")
+    public String getLikeMembers(@Positive @PathVariable("diary-id") long dairyId) {
+        return "Like click members!!";
+    }
+
+
+    /*
+    JWT를 이용한 로그인 정보 필요... 사용자가 로그인 된 사용자인가?  LoginMember  ..
+     */
+    @PostMapping("/{diary-id}")
+    public ResponseEntity<String>addLike (@PathVariable Long diaryId){
+
+        boolean result = false;
+
+        return "I Like this!";
+    }
+
+}
---
+++ server/danim/src/main/java/project/danim/likes/service/LikesService.java
@@ -0,0 +1,32 @@
+package project.danim.likes.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.danim.diary.domain.Diary;
+import project.danim.diary.service.DiaryService;
+import project.danim.likes.repository.LikesRepository;
+
+import javax.transaction.Transactional;
+
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class LikesService {
+
+    private final LikesRepository likesRepository;
+    private final DiaryService diaryService;
+
+    //@Transactional(readOnly = true)
+    public boolean isNotAlreadyLike(Member member, Diary diary){
+        return likesRepository.findByMemberAndDiary(member,diary).isEmpty();
+    }
+
+    public boolean addLike(Long memberId, Long diaryId){
+        Diary diary = diaryService.findDiary(diaryId);
+
+
+        return false;
+
+    }
+}
