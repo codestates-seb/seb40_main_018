4aae96fbf7d5f4939bbed8d69d6b727bbdede9ad
Im WonYong <ssassb123@daum.net>
2022-11-22 11:06:01 +0900

    [BE]fix : 개발 일정을 맞추기 위한 레이어드 형태로 다시 변경

--- server/danim/src/main/java/project/danim/member/application/port/in/CreateMemberUserCase.java
+++
@@ -1,7 +0,0 @@
-package project.danim.member.application.port.in;
-
-import project.danim.member.application.port.in.request.CreateMemberCommand;
-
-public interface CreateMemberUserCase {
-    void createMember(CreateMemberCommand createMemberCommand);
-}
--- server/danim/src/main/java/project/danim/member/application/port/in/MemberQueriesUseCase.java
+++
@@ -1,7 +0,0 @@
-package project.danim.member.application.port.in;
-
-import project.danim.member.application.port.in.response.MemberResponseForProfile;
-
-public interface MemberQueriesUseCase {
-    MemberResponseForProfile getMyProfile(String email);
-}
