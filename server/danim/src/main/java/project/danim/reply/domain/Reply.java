7b483d9e0e1e4a0817ab1803a716a4bc053f7cb7
2D3Edith <as13337@gmail.com>
2022-11-24 07:23:13 +0900

    Add: 댓글 엔티티 추가

--- server/danim/src/main/java/project/danim/reply/domain/Reply.java
+++ server/danim/src/main/java/project/danim/reply/domain/Reply.java
@@ -1,4 +1,31 @@
 package project.danim.reply.domain;
 
-public class Reply {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import project.danim.audit.BaseTime;
+
+import javax.persistence.*;
+
+@Entity(name = "REPLY")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Reply extends BaseTime {
+
+    @Id
+    @Column(name = "REPLY_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyId;
+
+    @Column(length = 500)
+    private String replyContent;
+
+    @Builder
+    public Reply(Long replyId, String replyContent) {
+        this.replyId = replyId;
+        this.replyContent = replyContent;
+    }
+
 }
