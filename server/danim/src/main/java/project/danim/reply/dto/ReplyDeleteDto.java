3ed83b9cb770a1d4f366e94ea8f1a75dc50341c0
2D3Edith <as13337@gmail.com>
2022-11-24 07:27:03 +0900

    Add: 댓글 DTO

--- server/danim/src/main/java/project/danim/reply/dto/ReplyDeleteDto.java
+++ server/danim/src/main/java/project/danim/reply/dto/ReplyDeleteDto.java
@@ -1,4 +1,13 @@
 package project.danim.reply.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ReplyDeleteDto {
+
+    private Long replyId;
+
+    public ReplyDeleteDto(Long replyId) {
+        this.replyId = replyId;
+    }
 }
--- server/danim/src/main/java/project/danim/reply/dto/ReplyResponseDto.java
+++ server/danim/src/main/java/project/danim/reply/dto/ReplyResponseDto.java
@@ -1,4 +1,29 @@
 package project.danim.reply.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.danim.bucket.domain.Bucket;
+import project.danim.bucket.dto.BucketResponseDto;
+import project.danim.reply.domain.Reply;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReplyResponseDto {
+
+    private Long replyId;
+
+    private String replyContent;
+
+    private LocalDateTime createdAt;
+
+    public static ReplyResponseDto of(Reply reply) {
+        return new ReplyResponseDto(reply.getReplyId(), reply.getReplyContent(), reply.getCreatedAt());
+    }
+
 }
