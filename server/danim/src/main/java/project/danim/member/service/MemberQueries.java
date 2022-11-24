8519445ff02c972e5e42af88fba5db3dcc4212c1
Im WonYong <ssassb123@daum.net>
2022-11-24 16:24:06 +0900

    [BE] add : member 조회를위한 queries

--- server/danim/src/main/java/project/danim/member/service/MemberQueries.java
+++ server/danim/src/main/java/project/danim/member/service/MemberQueries.java
@@ -1,33 +1,25 @@
-package project.danim.member.application.service;
+package project.danim.member.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import project.danim.exeption.ExceptionCode;
-import project.danim.member.adapter.out.persistence.MemberRepositoryPersistenceAdapter;
-import project.danim.member.application.port.in.MemberQueriesUseCase;
-import project.danim.member.application.port.in.response.MemberResponseForProfile;
 import project.danim.member.domain.Member;
 import project.danim.member.domain.MemberNotFoundException;
+import project.danim.member.dto.MemberResponseForProfile;
+import project.danim.member.repository.MemberRepository;
 
 @Component
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class MemberQueries implements MemberQueriesUseCase {
-    private final MemberRepositoryPersistenceAdapter memberPersistenceAdapter;
+public class MemberQueries {
+    private final MemberRepository memberRepository;
 
-    @Override
     public MemberResponseForProfile getMyProfile(String email) {
 
-        Member findMember = memberPersistenceAdapter.findByEmail(email)
+        Member findMember = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new MemberNotFoundException(ExceptionCode.MEMBER_NOT_FOUND));
 
-        return MemberResponseForProfile.builder()
-                .memberId(findMember.getMemberId())
-                .email(findMember.getEmail())
-                .nickname(findMember.getNickname())
-                .profileImg(findMember.getProfileImg())
-                .aboutMe(findMember.getAboutMe())
-                .build();
+        return MemberResponseForProfile.of(findMember);
     }
 }
