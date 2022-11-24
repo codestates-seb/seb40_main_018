eba20ea0708dbe7525cd577cd10fb56cce61612d
Im WonYong <ssassb123@daum.net>
2022-11-24 16:23:09 +0900

    [BE] add : member 입력과 응답을 위한 form

--- server/danim/src/main/java/project/danim/member/dto/MemberLoginForm.java
+++ server/danim/src/main/java/project/danim/member/dto/MemberLoginForm.java
@@ -1,22 +1,16 @@
 package project.danim.member.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-@Setter
 @Getter
-public class MemberCreateForm {
-
+public class MemberLoginForm {
     @NotBlank
     @Email
-    private String email;
+    public String email;
 
     @NotBlank
-    private String password;
-
-    @NotBlank
-    private String nickname;
+    public String password;
 }
