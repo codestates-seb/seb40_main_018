2e4136dcc3eeebfe6c84dda9706d31b7fbe7ab0c
youthmn <banghmin@hanmail.net>
2022-11-25 13:31:21 +0900

    ✨ 유저 수정기능 추가

--- client/src/components/UserEdit/UserEdit.js
+++ client/src/components/UserEdit/UserEdit.js
@@ -1,6 +1,8 @@
-// import { useState } from "react";
+import axios from "axios";
+import { useState } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 const Box = styled.div`
   background-color: white;
@@ -22,7 +24,6 @@ const Wrpper = styled.div`
 
 const UserImg = styled.div`
   background-color: white;
-  border: 1px solid red;
   border-radius: 100px;
   height: 120px;
   width: 120px;
@@ -30,16 +31,20 @@ const UserImg = styled.div`
   display: flex;
   justify-content: right;
   align-items: flex-end;
-  /* background-color: transparent; */
-  > .userImg {
-    background-color: transparent;
-    display: flex;
-  }
+  background-size: contain;
+  background-repeat: none;
+  background-color: transparent;
+  display: flex;
   > .icon {
     cursor: pointer;
     background-color: transparent;
-    color: #dadada;
+    height: 120px;
+    width: 120px;
   }
+`;
+
+const ImgInput = styled.input`
+  border: none;
 `;
 
 const UserInformation = styled.div`
@@ -47,64 +52,199 @@ const UserInformation = styled.div`
   background-color: white;
   display: flex;
   flex-direction: column;
-  > id {
-    background-color: white;
-    font-size: 14px;
-    margin: 26px 9px 0px 24px;
+`;
+
+const EditId = styled.input`
+  background-color: white;
+  font-size: 14px;
+  margin: 26px 9px 0px 24px;
+  padding: 5px;
+  width: 200px;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid gray;
+  &:focus-within {
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-bottom: 1px solid gray;
+    outline: none;
   }
-  > information {
-    background-color: white;
-    font-size: 14px;
-    margin: 22px 9px 0px 24px;
+`;
+
+const EditInformation = styled.input`
+  background-color: white;
+  font-size: 14px;
+  margin: 22px 9px 0px 24px;
+  padding: 5px;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid gray;
+  &:focus-within {
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-bottom: 1px solid gray;
+    outline: none;
   }
-  > count {
-    background-color: white;
-    font-size: 14px;
-    margin: 22px 9px 0px 24px;
-  }
+`;
+
+const Count = styled.div`
+  background-color: white;
+  font-size: 14px;
+  margin: 22px 9px 0px 24px;
+`;
+
+const Id = styled.div`
+  background-color: white;
+  font-size: 14px;
+  margin: 32px 9px 0px 24px;
+`;
+
+const Information = styled.div`
+  background-color: white;
+  font-size: 14px;
+  margin: 22px 9px 0px 24px;
 `;
 
 const Block = styled.div`
   display: flex;
   justify-content: flex-end;
   background-color: transparent;
-  > button {
-    width: 104px;
-    height: 30px;
-    color: white;
-    background-color: #a8a8a8;
-    box-shadow: rgba(0, 0, 0, 0.1) 4px 4px 12px;
-    font-size: 14px;
-    border: none;
-    border-radius: 35px;
-    margin: 0px 20px 14px 0px;
-    &:active {
-      background-color: #858585;
-    }
+`;
+
+const SubmitBtn = styled.button`
+  width: 104px;
+  height: 30px;
+  color: white;
+  background-color: #a8a8a8;
+  box-shadow: rgba(0, 0, 0, 0.1) 4px 4px 12px;
+  font-size: 14px;
+  border: none;
+  border-radius: 35px;
+  margin: 0px 20px 14px 0px;
+  &:active {
+    background-color: #858585;
   }
 `;
 
 export const UserEditBox = () => {
+  //상태를 만들어서 해주는거 어떨까요?
+  const defaultImg = "https://cdn-icons-png.flaticon.com/512/666/666201.png";
+
+  // const [userProfile, setUserProfile] = useState([]);
+  const [userNickname, setUserNickname] = useState("");
+  const [userIntro, setUserIntro] = useState("");
+  // const [diaryCount, setDiaryCount] = useState("1");
+  const [isEdit, setIsEdit] = useState(false);
+  const [img, setImg] = useState(defaultImg);
+
+  const editHandler = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const userNameHandler = (e) => {
+    setUserNickname(e.target.value);
+  };
+
+  const useInfoHandler = (e) => {
+    setUserIntro(e.target.value);
+  };
+
+  const submitHandler = () => {
+    setIsEdit(!isEdit);
+    const editUserProfile = {
+      userNickname: userNickname,
+      userIntro: userIntro,
+    };
+    axios.patch("http://localhost:4000/userProfile/1", editUserProfile).then((res) => console.log(res.data));
+  };
+
+  const cancelHandler = () => {
+    setIsEdit(!isEdit);
+    window.location.reload();
+  };
+
+  const onChangeFile = (e) => {
+    // console.log(e.target.files[0]);
+
+    let reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+    reader.onloadend = () => {
+      const resultImage = reader.result;
+      setImg(resultImage);
+    };
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/userProfile/1").then((res) => {
+      console.log(res.data);
+      // setUserProfile(res.data);
+      setUserNickname(res.data.userNickname);
+      setUserIntro(res.data.userIntro);
+    });
+  }, []);
+
   return (
     <Box>
-      <UserImg>
-        <div></div>
-        <FaRegEdit className="icon" />
-      </UserImg>
-      <Wrpper>
-        <UserInformation>
-          <id>youthmn</id>
-          <information>
-            24살 방혜민/ 프론트엔드 주니어 개발자 취준생24살 방혜민/ 프론트엔드 주니어 개발자 취준생24살 방혜민/
-            프론트엔드 주니어 개발자 취준생24살 방혜민/ 프론트엔드 주니어 개발자 취준생
-          </information>
-          <count>게시글 1</count>
-        </UserInformation>
-
-        <Block>
-          <button type="button">프로필 편집</button> <button type="button">수정하기</button> <button>취소하기</button>
-        </Block>
-      </Wrpper>
+      {isEdit ? (
+        // 편집할때 나와야할 내용
+        <>
+          <UserImg>
+            <img className="icon" src={img} alt="img" />
+            <ImgInput
+              type="file"
+              accept="image/jpg,impge/png,image/gif"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => onChangeFile(e)}
+            />
+            <label htmlFor="file">
+              <FaRegEdit className="icon"></FaRegEdit>{" "}
+            </label>
+          </UserImg>
+          <Wrpper>
+            <UserInformation>
+              <EditId onChange={userNameHandler} value={userNickname} />
+              <EditInformation onChange={useInfoHandler} value={userIntro} />
+              <Count></Count>
+            </UserInformation>
+            <Block>
+              <>
+                <SubmitBtn type="button" onClick={submitHandler}>
+                  저장하기
+                </SubmitBtn>{" "}
+                <SubmitBtn type="button" onClick={cancelHandler}>
+                  취소하기
+                </SubmitBtn>
+              </>
+            </Block>
+          </Wrpper>
+        </>
+      ) : (
+        <>
+          {/* 편집 완료됬을때 나올 화면 */}
+          <UserImg>
+            <img className="icon" src={img} alt="img" />
+          </UserImg>
+          <Wrpper>
+            <UserInformation>
+              <Id> {userNickname}</Id>
+              <Information> {userIntro}</Information>
+              <Count>게시글 1</Count>
+            </UserInformation>
+            <Block>
+              <SubmitBtn type="button" onClick={editHandler}>
+                프로필 편집
+              </SubmitBtn>
+            </Block>
+          </Wrpper>
+        </>
+      )}
     </Box>
   );
 };
