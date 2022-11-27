075e3d8658d24e7ac4d3033185c58e5917b14ceb
2D3Edith <as13337@gmail.com>
2022-11-27 19:55:51 +0900

    chore: AWS 임시 주석처리

--- server/danim/src/main/java/project/danim/image/FileStore.java
+++ server/danim/src/main/java/project/danim/image/FileStore.java
@@ -1,62 +1,63 @@
-package project.danim.image;
-
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import project.danim.exeption.BusinessLogicException;
-import project.danim.exeption.ExceptionCode;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-
-@Service
-@RequiredArgsConstructor
-public class AmazonService {
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    private final AmazonS3 s3;
-
-    public List<String> uploadImage(List<MultipartFile> multipartFile) {
-        List<String> fileNameList = new ArrayList<>();
-
-        multipartFile.forEach(file -> {
-            String fileName = createFileName(file.getOriginalFilename());
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
-            objectMetadata.setContentType(file.getContentType());
-
-            try(InputStream inputStream = file.getInputStream()) {
-                s3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
-                        .withCannedAcl(CannedAccessControlList.PublicRead));
-            } catch(IOException e) {
-                throw new BusinessLogicException(ExceptionCode.INTERNAL_SERVER_ERROR);
-            }
-
-            fileNameList.add(fileName);
-        });
-
-        return fileNameList;
-    }
-
-    public void deleteImage(String fileName) {
-        s3.deleteObject(new DeleteObjectRequest(bucket, fileName));
-    }
-
-    private String createFileName(String fileName) {
-        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
-    }
-
-    private String getFileExtension(String fileName) {
-        try {
-            return fileName.substring(fileName.lastIndexOf("."));
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new BusinessLogicException(ExceptionCode.BAD_REQUEST);
-        }
-    }
-}
+//package project.danim.image;
+//
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.model.*;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//import project.danim.exeption.BusinessLogicException;
+//import project.danim.exeption.ExceptionCode;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.util.*;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class FileStore {
+//
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String bucket;
+//
+//    private final AmazonS3 s3;
+//
+//    public List<String> uploadImage(List<MultipartFile> multipartFile) {
+//        List<String> fileNameList = new ArrayList<>();
+//
+//        multipartFile.forEach(file -> {
+//            String fileName = createFileName(file.getOriginalFilename());
+//            ObjectMetadata objectMetadata = new ObjectMetadata();
+//            objectMetadata.setContentLength(file.getSize());
+//            objectMetadata.setContentType(file.getContentType());
+//
+//            try(InputStream inputStream = file.getInputStream()) {
+//                s3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+//                        .withCannedAcl(CannedAccessControlList.PublicRead));
+//            } catch(IOException e) {
+//                throw new BusinessLogicException(ExceptionCode.INTERNAL_SERVER_ERROR);
+//            }
+//
+//            fileNameList.add(fileName);
+//        });
+//
+//        return fileNameList;
+//    }
+//
+//    public void deleteImage(String fileName) {
+//        s3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+//    }
+//
+//    private String createFileName(String fileName) {
+//        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+//    }
+//
+//    private String getFileExtension(String fileName) {
+//        try {
+//            return fileName.substring(fileName.lastIndexOf("."));
+//        } catch (StringIndexOutOfBoundsException e) {
+//            throw new BusinessLogicException(ExceptionCode.BAD_REQUEST);
+//        }
+//    }
+//}
