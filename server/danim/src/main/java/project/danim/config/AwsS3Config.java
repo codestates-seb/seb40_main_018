e726bb8942b5c769c0cf07e655d3f1bc4b5a4acc
2D3Edith <as13337@gmail.com>
2022-11-30 00:44:03 +0900

    chore: config 수정

--- server/danim/src/main/java/project/danim/config/AwsS3Config.java
+++ server/danim/src/main/java/project/danim/config/AwsS3Config.java
@@ -1,50 +1,36 @@
 package project.danim.config;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AwsS3Config {
-
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${iam.access-key}")
     private String accessKey;
 
-    @Value(("${cloud.aws.credentials.secret-key}"))
+    @Value("${iam.secret-key}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
-    @Primary
-    public BasicAWSCredentials awsCredentialsProvider(){
-        return new BasicAWSCredentials(accessKey, secretKey);
-    }
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String bucket;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(awsCredentialsProvider())
-                )
-                .build();
-    }
+    public AmazonS3 amazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-    //aws S3 접속
-    @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 
