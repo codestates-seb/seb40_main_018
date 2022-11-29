1e24dedd40cbafef703d9f591b80814e0116c756
2D3Edith <as13337@gmail.com>
2022-11-30 00:43:51 +0900

    Add: 이미지 올리기 로직 수정
    - 다이어리
    - 이미지

--- server/danim/src/main/java/project/danim/diary/service/DiaryService.java
+++ server/danim/src/main/java/project/danim/diary/service/DiaryService.java
@@ -1,32 +1,41 @@
 package project.danim.diary.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import project.danim.diary.domain.Diary;
 import project.danim.diary.dto.DiaryResponseDto;
 import project.danim.diary.repository.DiaryRepository;
 import project.danim.exeption.BusinessLogicException;
 import project.danim.exeption.ExceptionCode;
+import project.danim.image.AwsS3Service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class DiaryService {
     private final DiaryRepository diaryRepository;
 
-    public DiaryService(DiaryRepository diaryRepository) {
+    private final AwsS3Service awsS3Service;
+
+    public DiaryService(DiaryRepository diaryRepository, AwsS3Service awsS3Service) {
         this.diaryRepository = diaryRepository;
+        this.awsS3Service = awsS3Service;
     }
 
-    public Diary createDiary(Diary diary){
+    public Diary createDiary(Diary diary) {
         return diaryRepository.save(diary);
     }
 
-    public Diary findVerifiedDiary(long diaryId){
+    public Diary findVerifiedDiary(long diaryId) {
         Optional<Diary> optionalDiary = diaryRepository.findById(diaryId);
-        Diary findDiary = optionalDiary.orElseThrow(()->
+        Diary findDiary = optionalDiary.orElseThrow(() ->
                 new BusinessLogicException(ExceptionCode.DIARY_NOT_FOUND));
         return findDiary;
     }
@@ -45,15 +54,16 @@ public class DiaryService {
         return (List<Diary>) diaryRepository.findAll();
     }
 
-   public void savedLikesCount(Diary diary){
+    public void savedLikesCount(Diary diary) {
         diaryRepository.save(diary);
     }
+
     /*
 
     다이어리 수정
 
      */
-    public Diary updateDiary(Diary diary){
+    public Diary updateDiary(Diary diary) {
 
         Diary findDiary = findVerifiedDiary(diary.getDiaryId()); // 요청된 일기가 DB에 없으면 에러
         //diaryRepository.findById(diaryId);
@@ -67,7 +77,7 @@ public class DiaryService {
         Optional.ofNullable(diary.getCost())
                 .ifPresent(diaryCost -> findDiary.setCost(diaryCost));
 
-      //  findDiary.setModifiedDate(LocalDateTime.now());
+        //  findDiary.setModifiedDate(LocalDateTime.now());
 
         Diary updatedDiaries = diaryRepository.save(findDiary);
 
@@ -85,10 +95,20 @@ public class DiaryService {
     /*
     특정 다이어리 삭제
      */
-    public void deleteDiary(long diaryId){
+    public void deleteDiary(long diaryId) {
         diaryRepository.deleteById(diaryId);
     }
 
+    public Long keepDiary(MultipartFile image, Diary diary) throws IOException {
 
+        if (!image.isEmpty()) {
+            String storedFileName = awsS3Service.upload(image, "images");
+            diary.setImageUrl(storedFileName);
+        }
 
+        Diary savedDiary = diaryRepository.save(diary);
+
+        return savedDiary.getDiaryId();
+
+    }
 }
