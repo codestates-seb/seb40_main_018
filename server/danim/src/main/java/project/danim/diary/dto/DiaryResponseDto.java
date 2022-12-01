fe568697599edbbc1046e14cc510e1b705565b43
hyeonayou <Youhyeona99@gmail.com>
2022-12-01 23:09:24 +0900

    add : city , tags Response 추가

--- server/danim/src/main/java/project/danim/diary/dto/DiaryResponseDto.java
+++ server/danim/src/main/java/project/danim/diary/dto/DiaryResponseDto.java
@@ -3,9 +3,11 @@ package project.danim.diary.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import project.danim.tag.domain.Tag;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,7 +16,9 @@ public class DiaryResponseDto {
     private String title;
     private String content;
     private String weather;
+    private String city;
     private String area;
+    private List<Tag> tags;
     private int cost;
     private long memberId;
     private long likeCount;
